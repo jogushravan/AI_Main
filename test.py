7254f2e0b3b2c842b66c7c5a696b99e734929d19
Shravanjogu <jogu.shravan@gmail.com>
2022-05-01 00:39:58 -0400

    done

---
+++ test.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set()
+df = pd.read_csv("C:\Users\Diya\Downloads\ML by JosePortilla\05-Seaborn\dm_office_sales.csv")
+df.head()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
