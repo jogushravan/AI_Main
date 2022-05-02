9ed12e3f4d4b0f672cad9e88eda30497985953e5
Shravanjogu <jogu.shravan@gmail.com>
2022-05-01 20:09:06 -0400

    d

--- test.py
+++ test.py
@@ -2,19 +2,21 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from mpl_toolkits.mplot3d import Axes3D
 
 df = pd.read_csv("Visualization\dm_office_sales.csv")
 df.head()
 
 
-
-
-plt.figure(figsize=(10,4),dpi=200)
-# Paired would be a good choice if there was a distinct jump from 0 and 1 to 2 and 3
-#sns.countplot(x='level of education',data=df,hue='training level',palette='Paired')
-sns.barplot(x='level of education',data=df,y='salary',hue='training level',palette='Paired', estimator=np.mean)
-
-sns.countplot(x='level of education',data=df,hue='training level',palette='Set1')
+file = pd.read_csv("vgsales.csv")
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+x = file['NA_Sales']
+y = file['EU_Sales']
+z = file['Other_Sales']
+ax.scatter(x, y, z, c='r', s=20)
+plt.xticks(rotation=60)
+plt.show()
 
 
 
